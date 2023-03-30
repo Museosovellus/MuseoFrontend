c4d1e4476b0fa7ca275df248a7a19963ab172aeb
Lauri Reis <reislauri@gmail.com>
2023-03-30 13:08:53 +0300

    Yksikkötestejä joka screenille

---
+++ __tests__/screens/MuseoList.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react-native";
+import { ListScreen } from "../../Screens/MuseoList";
+
+test('Renders the <ListScreen /> component', () => {
+  render(<ListScreen />);
+});
